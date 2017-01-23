194bd6e4129d57c90d3c7e5c06b0b2e082362ba5
Philneeves <p.neeves@gmail.com>
2017-01-23 17:06:42 +0000

    numbers of changes and images added

--- js/philjquery.js
+++ js/philjquery.js
@@ -22,6 +22,31 @@ $(window).on("load", function(){
     });
 });
 
+$("#imgtn1").hover(function() {
+$( '#imgtn1' ).attr("src","img/ang2.png");
+}, 
+function() {
+});
+
+$("#imgtn2").hover(function() {
+$( '#imgtn2' ).attr("src","img/rps_new.png");
+}, 
+function() {
+});
+
+// $(document).ready(function(){
+$("#imgtn3").hover(function() {
+$( '#imgtn3' ).attr("src","img/album_banner1.jpg");
+}, 
+function() {
+});
+
+$("#imgtn4").hover(function() {
+$( '#imgtn4' ).attr("src","img/me_icon.jpeg");
+}, 
+function() {
+});
+
 
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();   
